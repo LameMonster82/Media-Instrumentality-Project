7bbdc36e96147c3d345c318cd43019f88c2badd2
cake <code@cakey.me>
2026-05-21 22:00:51 +0200

    FEAT: Reworked some dir structure

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { initLibrary } from "./flat/FlatEntrypoint";
+
+function xr_entrypoint(xr_session: XRSession) {
+  console.log("XR Mode");
+}
+
+async function flat_entrypoint() {
+  console.log("Flat Mode");
+  await initLibrary();
+}
+
+// --------- actual code ----------- //
+
+let xr = navigator.xr;
+
+if (xr) {
+  xr.requestSession("immersive-vr").then((session) => {
+    xr_entrypoint(session);
+  });
+}
+else flat_entrypoint();
